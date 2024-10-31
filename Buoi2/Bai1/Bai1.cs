8d1a55859005efa241f74c773f0dbb6a2cee5839
Hieu Day <hieukaj@gmail.com>
2024-10-31 23:35:06 +0700

    BTVN buoi 2

---
+++ Buoi2/Bai1/Bai1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Bai1
+{
+    internal class Bai1
+    {
+        static double CalculateDistance(double A, double b, double t)
+        {
+            double T = 2.0 / b; 
+            int vongHoanThanh = (int)(t / T);
+            double tConLai = t % T;
+
+            return Math.Round(vongHoanThanh * 4 * A + 2 * A * Math.Abs(Math.Cos(b * Math.PI * tConLai + Math.PI / 2)), 3);
+        }
+
+        static void Main(String [] args)
+        {
+            //Bai 1 a
+            Console.WriteLine($"Quang duong vat A di duoc trong 2.125 giay la: {CalculateDistance(9, 5, 2.125)}cm ");
+
+            //Bai 1 b
+            Console.WriteLine("Phuong trinh dao dong B: x(t)= Acos(bπt+π/2) (cm)");
+            Console.Write("Nhap bien do cua vat dao dong B: ");
+            double A = double.Parse(Console.ReadLine());
+            Console.Write("Nhap omega cua vat dao dong B = ?π: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Nhap thoi gian dao dong cua vat: ");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Quang Duong vat B di chuyen duoc sau {t}s la: {CalculateDistance(A, b, t)}cm");
+        }
+
+    }
+}
---
+++ Buoi2/Bai2/Bai2/Bai2.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Bai2;
+internal class Bai2
+{
+    static void Main()
+    {   
+        double v0 = 20; 
+        double theta = 30; 
+        double g = 9.8; 
+
+        double thetaRad = theta * Math.PI / 180;
+
+        double vx = v0 * Math.Cos(thetaRad);
+        double vy = v0 * Math.Sin(thetaRad);
+
+        Console.WriteLine($"Van toc ban dau theo phuong ngang: {vx} m/s");
+        Console.WriteLine($"Van toc ban dau theo phuong thang dung: {vy} m/s");
+
+        double tMax = vy / g;
+        Console.WriteLine($"Thoi gian len den diem cao nhat la: {tMax} s");
+       
+        double hMax = (vy * vy) / (2 * g);
+        Console.WriteLine($"Chieu cao cuc dai: {hMax} m");
+     
+        double totalTime = 2 * tMax; 
+        double d = vx * totalTime;
+        Console.WriteLine($"Quang duong ngang: {d} m");
+    }
+}
